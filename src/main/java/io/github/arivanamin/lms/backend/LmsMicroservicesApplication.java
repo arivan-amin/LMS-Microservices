4d310c7bc9d33d900013547fabcf5f048c52ba5a
Arivan Amin <arivanamin@gmail.com>
2025-10-05 21:54:30 +0300

    Initial commit

---
+++ src/main/java/io/github/arivanamin/lms/backend/LmsMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package io.github.arivanamin.lms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsMicroservicesApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(LmsMicroservicesApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/arivanamin/lms/backend/LmsMicroservicesApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.arivanamin.lms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+}
