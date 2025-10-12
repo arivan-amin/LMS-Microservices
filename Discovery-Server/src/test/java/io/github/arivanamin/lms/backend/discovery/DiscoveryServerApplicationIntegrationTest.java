6c45ad807b28e34488ff6a7859d9a2da3216f647
Arivan Amin <arivanamin@gmail.com>
2025-10-12 10:36:50 +0300

    Add Eureka discovery server module

---
+++ Discovery-Server/src/test/java/io/github/arivanamin/lms/backend/discovery/DiscoveryServerApplicationIntegrationTest.java
@@ -0,0 +1,23 @@
+package io.github.arivanamin.lms.backend.discovery;
+
+import io.github.arivanamin.lms.backend.testing.architecture.bases.BaseUnitTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Slf4j
+@SpringBootTest
+@ExtendWith (SpringExtension.class)
+class DiscoveryServerApplicationIntegrationTest implements BaseUnitTest {
+
+    @Test
+    void mainShouldInitializeContext () {
+        // given
+
+        // when
+
+        // then
+    }
+}
